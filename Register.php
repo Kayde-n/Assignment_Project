d2ca0718a6ca7b444394949b3501fdd5a47d229e
Kayden <wkengyew2006@gmail.com>
2025-11-28 13:22:01 +0800

    Register.php

---
+++ Register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EcoXP Register Page</title>
+</head>
+
+<body>
+    <div>
+        <h1>Hello!</h1>
+    </div>
+    <form action="POST">
+        <div>
+            <label>Username</label>
+            <input type="text" id="username" name="username" placeholder="Enter your username..." required>
+        </div>
+        <div>
+            <label>Email Address</label>
+            <input type="email" id="email" name="email" placeholder="Enter your email..." required>
+        </div>
+
+        <div>
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Enter your password..." required>
+        </div>
+        <div>
+            <label>Con</label>
+        </div>
+
+        <button type="submit">Log in</button>
+        <button type="button">Sign up</button>
+    </form>
+    <div>
+        <img src="Images/EcoXP_logo.png">
+        <h2>EcoXP</h2>
+    </div>
+
+</body>
+
+</html>
