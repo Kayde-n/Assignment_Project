c3b55e31c4e905c3cbeacbd1054d77d51d6cb689
Kayden <wkengyew2006@gmail.com>
2025-11-28 13:06:40 +0800

    added

---
+++ Login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EcoXP Login Page</title>
+</head>
+
+<body>
+    <div>
+        <h1>Welcome Back!</h1>
+    </div>
+    <form action="POST">
+        <div>
+            <label>Email Address</label>
+            <input type="email" id="email" name="email" placeholder="Enter your email..." required>
+        </div>
+        <div>
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Enter your password..." required>
+        </div>
+
+        <button type="submit">Log in</button>
+        <button type="button">Sign up</button>
+    </form>
+    <div>
+        <img src="Images/EcoXP_logo.png">
+        <h2>EcoXP</h2>
+    </div>
+
+</body>
+
+</html>
