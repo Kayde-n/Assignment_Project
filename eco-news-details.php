c4959e28a103949ecb099cdc14872d89d3299e73
Kayden <wkengyew2006@gmail.com>
2025-12-03 14:23:12 +0800

    Added eco news details page

---
+++ eco-news-details.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News Details</title>
+</head>
+
+<body>
+    <button>&larr;</button>
+    <div>
+        <img src="" alt="Image PLaceholder">
+    </div>
+
+
+    <p><strong>Community</strong></p>
+
+    <h2>Top 5 Green Tips for reducing e-waste.</h2>
+
+    <p>
+        UK businesses and households produce 1.45 million tonnes of e-waste a year,
+        equating to 23.9kg of e-waste each year per capita, which places the UK as
+        the second highest producer in the world.
+    </p>
+
+    <h3>What is e-waste?</h3>
+
+    <p>
+        Electronic waste, or e-waste, refers to any product with electrical components
+        that businesses and households dispose of, including laptops, tablets, phones,
+        televisions, air conditioners and printers. The proliferation of electronic
+        devices among businesses and households is causing a major environmental problem
+        since unwanted electronics are often sent to landfill with little consideration
+        for the reusable resources they contain.
+    </p>
+
+    <nav>
+        <button>Home</button>
+        <button>Tools</button>
+        <button>Scan</button>
+        <button>Messages</button>
+        <button>Profile</button>
+    </nav>
+
+</body>
+
+</html>
