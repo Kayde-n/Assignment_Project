c28f4dba5589dd5de4f99b848699e1ae4f419410
VinceyLincey <heong.vince@gmail.com>
2026-01-04 22:54:18 +0800

    faq html files saja

---
+++ bug-report.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Report a Bug</title>
+</head>
+<body>
+    <h1>
+        I think there is a bug in the app  D:
+    </h1>
+
+    <p>
+        If you encounter any issues, please report the problem with details such as screenshot and steps to reproduce the bug.
+    </p>
+
+    <a href="participants-desktop-F&Q.php">Back to Help & FAQ</a>
+
+</body>
+</html>
---
+++ verification-time.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verification Time</title>
+</head>
+<body>
+    <h1>
+        How long does verification take?
+    </h1>
+
+    
+    <p>
+        Verification usually takes between 1 to 3 working days,
+        depending on the activity submitted.
+    </p>
+
+    <a href="participants-desktop-F&Q.php">Back to Help & FAQ</a>
+
+</body>
+</html>
