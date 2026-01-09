ad2bab2fcd127ed61500801ecdd80a7ce06754d2
VinceyLincey <heong.vince@gmail.com>
2026-01-10 02:23:36 +0800

    changed the login directory when no user id it dosent say url not found

--- session.php
+++ session.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
     if(!isset($_SESSION['mySession'])){
-        echo "<script>alert('Please login first.');window.location.href='Login.php';</script>";
+        echo "<script>alert('Please login first.');window.location.href='../../Login.php';</script>";
     }
 ?>
